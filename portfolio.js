fc28743406c4eacbcb86e78150e95870cdc43085
Chroma4 <86936379+Chroma4@users.noreply.github.com>
2021-07-20 11:05:53 +0900

    背景アニメーション追加

--- portfolio.js
+++ portfolio.js
@@ -18,10 +18,3 @@ $(function() {
 
     });
   });
-
-  var particles = Particles.init({
-	selector: '.background',
-  sizeVariations: 10,
-  color: ['#00bbdd', '#404B69', '#DBEDF3'],
-  connectParticles: true
-});
